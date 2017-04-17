0aeb51774ac022585cc9e95828c2159094f56b3c
Kyle Feng <kylefeng28@gmail.com>
2017-04-17 18:24:01 -0400

    Add some mad beats yo

--- js/MainGame.js
+++ js/MainGame.js
@@ -204,6 +204,22 @@ RhythmArena.MainGame.prototype = {
         //this.camera.focusOnXY(0, 0);
         //this.camera.follow(this.players.p1.sprite);
         this.camera.setBoundsToWorld();
+
+		// Play some beats yo
+
+		Gibber.init(); // Required for Gibber
+		Clock.bpm = this.music.bpm;
+		Clock.rate = 1;
+		Gibber.scale.root.seq( ['c4','eb4'], 2);
+		
+		a = Mono('bass').note.seq( [0,7], 1/8 );
+		
+		b = EDrums('xoxo');
+		b.snare.snappy = 1;
+
+		c = Mono('easyfx')
+		  .note.seq( Rndi(0,12), [1/4,1/8,1/2,1,2].rnd( 1/8,4 ) );
+
     },
     posMod: function(a, n) {
         /*console.log(a, " mod ", n);*/
