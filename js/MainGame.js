45712155dd74f6260a260432ec7ceefa45aaa6dd
Kyle Feng <kylefeng28@gmail.com>
2017-04-17 20:08:31 -0400

    Add some glitchy beats

--- js/MainGame.js
+++ js/MainGame.js
@@ -212,13 +212,8 @@ RhythmArena.MainGame.prototype = {
 		Clock.rate = 1;
 		Gibber.scale.root.seq( ['c4','eb4'], 2);
 		
-		a = Mono('bass').note.seq( [0,7], 1/8 );
-		
-		b = EDrums('xoxo');
-		b.snare.snappy = 1;
-
-		c = Mono('easyfx')
-		  .note.seq( Rndi(0,12), [1/4,1/8,1/2,1,2].rnd( 1/8,4 ) );
+		// GibberSongs.basic();
+		GibberSongs.glitchy();
 
     },
     posMod: function(a, n) {
